10a3a50aa67fbeac2cedacab63b60cefadd08ff3
Nithakm8 <nithakm8@gmail.com>
2025-09-19 22:32:04 +0530

    bloodbank

--- clientside/js/index.js
+++ clientside/js/index.js
@@ -1,1 +1,22 @@
-console.log('hai');
+let donars=[]
+async function getDonar(){
+    const res=await fetch('http://localhost:4000/getDonar')
+    const data=await res.json()
+    donars=data
+    let str=''
+   donars.forEach((donar)=>{
+        str+=`
+        <div class="donar">
+      <h3>Name:${donar.name}</h3>
+      <h3>BloodType:${donar.bloodtype}</h3>
+      <h3>Age:${donar.age}</h3>
+      <h3>Weight:${donar.weight}</h3>
+      <h3>Phone Number:${donar.phnumber}</h3>
+      <h3>Last Donated Date:${donar.date}</h3>
+    </div>
+    `
+    })
+    document.getElementById('donars').innerHTML=str  
+    
+}
+getDonar()
--- serverside/server.js
+++ serverside/server.js
@@ -1,7 +1,12 @@
 const fs=require('fs')
 const http=require('http')
 const url=require('url')
-const app=http.createServer((req,res)=>{
+const {MongoClient}=require('mongodb')
+const queryString=require('querystring')
+const client=new MongoClient('mongodb://127.0.0.1:27017/')
+const app=http.createServer(async(req,res)=>{
+    const db=client.db('BloodBank')
+    const collection=db.collection('donar')
     const {pathname}=url.parse(req.url)
     if(pathname=='/'){
         res.writeHead(200,{"content-Type":"text/html"})
@@ -14,6 +19,35 @@ const app=http.createServer((req,res)=>{
     else if(pathname=='/pages/add.html'){
          res.writeHead(200,{"content-Type":"text/html"})
         res.end(fs.readFileSync('../clientside/pages/add.html'))
+    }
+    if(pathname=='/submit' && req.method=='POST'){
+        let body=''
+        req.on('data',(chunks)=>{
+            body+=chunks.toString()
+            console.log(body);
+            
+        })
+        req.on('end',()=>{
+            const formData=queryString.parse(body)
+            collection.insertOne(formData).then(()=>{
+                console.log('Data added');
+                
+            }).catch((err)=>{
+                console.log(err);
+                
+            })
+        })
+        res.writeHead(200,{'content-type':"text/html"})
+        res.end(fs.readFileSync('../clientside/index.html'))
+    }
+    if(pathname=='/getDonar' && req.method=='GET'){
+        const data=await collection.find().toArray()
+        console.log(data);
+        const jsonData=JSON.stringify(data)
+        console.log(jsonData);
+        
+        res.writeHead(200,{'content-type':"text/json"})  
+        res.end(jsonData)      
     }
 })
 app.listen(4000)
